167c7fec4bfb9bed505ac4ca4fa1efd80229b1ad
NtaVi <66380769+ntavi@users.noreply.github.com>
2026-06-29 16:45:19 +0300

    Update wrap-wine.sh

--- scripts/wrap-wine.sh
+++ scripts/wrap-wine.sh
@@ -4,16 +4,16 @@ set -euxo pipefail
 # Install wrapper script for box86 & box64
 cat << EOF > /usr/local/bin/wine
 #!/bin/sh
-WINEPREFIX=~/.wine WINEARCH=win32 box86 ~/wine/bin/wine \$@
+WINEPREFIX=\${WINEPREFIX:-~/.wine} WINEARCH=win32 box86 /opt/wine/bin/wine \$@
 EOF
 cat << EOF > /usr/local/bin/wine64
 #!/bin/sh
-WINEPREFIX=~/.wine64 WINEARCH=win64 box64 ~/wine/bin/wine64 \$@
+WINEPREFIX=\${WINEPREFIX:-~/.wine64} WINEARCH=win64 box64 /opt/wine/bin/wine64 \$@
 EOF
 cat << EOF > /usr/local/bin/wineserver
 #!/bin/sh
-WINEPREFIX=~/.wine64 WINEARCH=win64 box64 ~/wine/bin/wineserver \$@
+WINEPREFIX=\${WINEPREFIX:-~/.wine64} WINEARCH=win64 box64 /opt/wine/bin/wineserver \$@
 EOF
-ln -s ~/wine/bin/wineboot /usr/local/bin/wineboot
-ln -s ~/wine/bin/winecfg /usr/local/bin/winecfg
+ln -sf /opt/wine/bin/wineboot /usr/local/bin/wineboot
+ln -sf /opt/wine/bin/winecfg /usr/local/bin/winecfg
 chmod +x /usr/local/bin/wine /usr/local/bin/wine64 /usr/local/bin/wineboot /usr/local/bin/winecfg /usr/local/bin/wineserver
